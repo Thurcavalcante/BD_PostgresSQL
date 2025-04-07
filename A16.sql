00a78ad1f634581cab3fcbf166f75f81794d8eb9
Thurcavalcante <cavalcantethur@gmail.com>
2025-04-07 17:35:16 -0300

    feat: (Arquivo A16) Comando RIGHT JOIN

---
+++ A16.sql
@@ -0,0 +1,8 @@
+-- A16 Comando RIGHT JOIN
+
+-- * Usamos o comando RIGHT JOIN quando precisamos criar um SELECT que busca os dados em mais de uma tabela.
+-- * Na aula anterior aprendemos a usar o comando LEFT JOIN que tem o mesmo objetivo.
+-- * Agora vamos ver na prática como o RIGHT JOIN funciona.
+
+-- * Estrutura da sintaxe:
+select * from tabela_a left join tabela_b on condicao_p_juncao;
