4adbe705b027f96de25e4bc81a351f6662a220ef
Thurcavalcante <cavalcantethur@gmail.com>
2025-04-07 16:43:37 -0300

    feat: (Arquivo A14) Comando INNER JOIN

---
+++ A14.sql
@@ -0,0 +1,7 @@
+-- A14 Comando INNER JOIN
+
+-- * Usamoso comando INNER JOIN quando precisamos criar um SELECT que busca os dados em mais de uma tabela.
+-- Estrutura do comando:
+select * from tabela_a inner join tabela_b on condicao_p_juncao;
+
+
